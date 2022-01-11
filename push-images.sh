61558e26d5b49076aa3a3cd22cdbf800a9842c33
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2022-01-11 11:20:19 -0500

    Bump to v16.13.2

--- push-images.sh
+++ push-images.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+docker push cimg/node:16.13.2
+docker push cimg/node:16.13
+docker push cimg/node:lts
+docker push cimg/node:16.13.2-browsers
+docker push cimg/node:16.13-browsers
+docker push cimg/node:lts-browsers
